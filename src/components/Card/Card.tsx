9eb529d97f8a7ae33a6a16e481657be9449234ba
Mikhail <mihkailmatveichuk@gmail.com>
2023-03-11 13:10:44 +0300

    feat: add description for cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,29 +2,52 @@ import React, { Component } from 'react';
 import './Card.scss';
 
 class Card extends Component {
+  state = {
+    toggle: false,
+    id: '',
+  };
+
+  onToggleDesc = (e: { target: { parentNode: { id: string } } }) => {
+    this.setState(({ toggle }) => ({
+      toggle: !toggle,
+      id: e.target.parentNode.id,
+    }));
+  };
+
   render() {
     const { id, title, rating, price, thumbnail } = this.props.item;
+    const style = this.state.toggle ? `product-desc__active` : 'product-desc';
+    const safeDesc = (id: string) => {
+      if (this.props.item.id === id) {
+        return this.props.item.description;
+      }
+    };
     return (
-      <div className="product-item" key={id}>
-        <img src={thumbnail} alt={title} />
-        <div className="product-list">
-          <h3>{title}</h3>
-          <span className="price">{price + '$'}</span>
-          <p>
-            <span
-              style={{
-                fontWeight: 'bold',
-              }}
-            >
-              Rating:
-            </span>{' '}
-            {rating + ' ★'}
-          </p>
-          <a href="" className="button">
-            В корзину
-          </a>
+      <>
+        <div className="product-item" key={id}>
+          <img src={thumbnail} alt={title} />
+          <div className="product-list" id={id}>
+            <h3>{title}</h3>
+            <span className="price">{price + '$'}</span>
+            <p>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                }}
+              >
+                Rating:
+              </span>{' '}
+              {rating + ' ★'}
+            </p>
+            <div className={style}>
+              <span>{safeDesc(id)}</span>
+            </div>
+            <button className="button" onClick={this.onToggleDesc}>
+              Description
+            </button>
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
