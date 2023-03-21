a1ef403cc69b35d170d55896f58a67e12145d80f
Mikhail <mihkailmatveichuk@gmail.com>
2023-03-21 17:49:31 +0300

    refactor: delete unnecessary toggle for link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Search from '../Search/Search';
-import { Link } from 'react-router-dom';
-import { SyntheticHeader } from 'type';
+import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
 class Header extends Component<
@@ -13,46 +12,15 @@ class Header extends Component<
     about: false,
   };
 
-  onToggleActive = (e: SyntheticHeader) => {
-    if (e.target.id == 'about') {
-      this.setState({
-        home: false,
-        about: true,
-      });
-    } else if (e.target.id == 'home') {
-      this.setState({
-        home: true,
-        about: false,
-      });
-    }
-  };
-
   render() {
-    const { home, about } = this.state;
     const { onUpdateSearch, value } = this.props;
-
-    const buttonsData = [
-      { id: 'home', label: 'Home', active: home, url: '' },
-      { id: 'about', label: 'About us', active: about, url: 'about' },
-    ];
-
-    const buttons = buttonsData.map((button, i) => {
-      return (
-        <Link
-          to={`/${button.url}`}
-          className={button.active ? 'active' : undefined}
-          id={button.id}
-          key={i}
-          onClick={this.onToggleActive}
-        >
-          {button.label}
-        </Link>
-      );
-    });
 
     return (
       <div className="header-container">
-        <div className="top-nav">{buttons}</div>
+        <div className="top-nav">
+          <NavLink to={`/`}>Home</NavLink>
+          <NavLink to={`/about`}>About us</NavLink>
+        </div>
         <div className="bottom-nav">
           <Search onUpdateSearch={onUpdateSearch} value={value} />
         </div>
