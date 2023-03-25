ef227d4720bda29759dfef938ef3474bfa1bd3e7
Mikhail <mihkailmatveichuk@gmail.com>
2023-03-25 13:28:35 +0300

    refactor: change App component from class to function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { data } from './data';
 import Header from './components/Header/Header';
@@ -7,24 +7,19 @@ import About from './components/About/About';
 import Error from './components/Error/Error';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import { Footer } from './components/Footer/Footer';
+import Forms, { IFormsState } from './components/Forms/Forms';
 import { ICards } from 'type';
 import './App.scss';
-import Forms, { IFormsState } from './components/Forms/Forms';
 
-class App extends Component<{ data: Array<ICards>; term: string }> {
-  state = {
-    data,
-    term: '',
-  };
+const App = () => {
+  const [term, setTerm] = useState('');
 
-  componentDidMount(): void {
-    const value = localStorage.getItem('value');
-    this.setState(() => ({
-      term: value,
-    }));
-  }
+  useEffect(() => {
+    const value: string | null = localStorage.getItem('value');
+    setTerm(value || '');
+  }, []);
 
-  searchEmp = (items: Array<ICards>, term: string) => {
+  const searchEmp = (items: Array<ICards>, term: string) => {
     if (!term) {
       return items;
     } else {
@@ -39,36 +34,33 @@ class App extends Component<{ data: Array<ICards>; term: string }> {
     }
   };
 
-  onUpdateSearch = (term: string) => {
-    this.setState({ term });
+  const onUpdateSearch = (term: string) => {
+    setTerm(term);
     localStorage.setItem('value', term);
   };
 
-  render() {
-    const { data, term } = this.state;
-    const visibleData = this.searchEmp(data, term);
-    const formsState: IFormsState = {
-      data: [
-        { file: '', name: 'John Doe', birthday: '', select: '', switcher: false, checkbox: false },
-      ],
-      errorName: false,
-      errorBirthday: false,
-    };
-    return (
-      <div className="main-page">
-        <ErrorBoundary>
-          <Header onUpdateSearch={this.onUpdateSearch} value={term} />
-          <Routes>
-            <Route path="/" element={<CardsField data={visibleData} />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<Error />} />
-            <Route path="/form" element={<Forms {...formsState} />} />
-          </Routes>
-          <Footer />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
+  const visibleData = searchEmp(data, term);
+  const formsState: IFormsState = {
+    data: [
+      { file: '', name: 'John Doe', birthday: '', select: '', switcher: false, checkbox: false },
+    ],
+    errorName: false,
+    errorBirthday: false,
+  };
+  return (
+    <div className="main-page">
+      <ErrorBoundary>
+        <Header onUpdateSearch={onUpdateSearch} value={term} />
+        <Routes>
+          <Route path="/" element={<CardsField data={visibleData} />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+          <Route path="/form" element={<Forms {...formsState} />} />
+        </Routes>
+        <Footer />
+      </ErrorBoundary>
+    </div>
+  );
+};
 
 export default App;
